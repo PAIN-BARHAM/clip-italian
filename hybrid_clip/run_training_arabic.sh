4f054d2630a940cc40c87d3bd5c978b062392594
PAIN-BARHAM <mohammedbrham98@gmail.com>
2023-05-23 09:18:25 +0400

    Adding files

--- hybrid_clip/run_training_arabic.sh
+++ hybrid_clip/run_training_arabic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=.
-MODEL_DIR=model_allcaption
+MODEL_DIR=/home/think3/Desktop/training_CLIP/model_allcaptions
 
 IMAGE_ENCODER="openai/clip-vit-base-patch32"
 TEXT_ENCODER="aubmindlab/bert-large-arabertv2"
@@ -9,18 +9,19 @@ TEXT_ENCODER="aubmindlab/bert-large-arabertv2"
 
 python ${SCRIPT_DIR}/run_hybrid_clip.py \
     --output_dir ${MODEL_DIR} \
+    --overwrite_output_dir \
     --tokenizer_name=${TEXT_ENCODER} \
     --train_file="/home/think3/Desktop/training_CLIP/MSCOCO_DATASET_AR_1/train_dataset.json" \
     --validation_file="/home/think3/Desktop/training_CLIP/MSCOCO_DATASET_AR_1/valid_dataset.json" \
     --do_train --do_eval \
     --num_train_epochs="80" --max_seq_length 128 \
-    --per_device_train_batch_size="128" \
-    --per_device_eval_batch_size="128" \
-    --learning_rate="0.00008" --warmup_ratio 0.1 --weight_decay 0.0 \
+    --per_device_train_batch_size="64" \
+    --per_device_eval_batch_size="64" \
+    --learning_rate="0.00001" --warmup_ratio 0.1 --weight_decay 0.0 \
     --preprocessing_num_workers 32 \
     --log_comet \
     --eval_when 5 \
     --text_model_name_or_path=${TEXT_ENCODER} \
     --vision_model_name_or_path=${IMAGE_ENCODER} \
-    --run_from_checkpoint /home/think3/Desktop/training_CLIP/model_allcaptions/
+    --run_from_checkpoint="/home/think3/Desktop/training_CLIP/model_allcaptions/39"
 
